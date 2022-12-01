ebcc19153e8dd0616f38c66caae1ac26e4dddd3a
Hieu <emerald.hieu@gmail.com>
2022-12-01 17:28:36 +0700

    Remove redundant annotations

--- order/src/main/java/com/emeraldhieu/vinci/order/OrderApp.java
+++ order/src/main/java/com/emeraldhieu/vinci/order/OrderApp.java
@@ -4,19 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.retry.annotation.EnableRetry;
 
-@EnableFeignClients
 /**
  * Scan property classes annotated with {@link ConfigurationProperties}.
  * See https://www.baeldung.com/configuration-properties-in-spring-boot#1-spring-boot-22
  */
 @ConfigurationPropertiesScan(basePackageClasses = {OrderApp.class})
 @SpringBootApplication
-@EnableRetry
-@EnableJpaAuditing
 public class OrderApp {
 
     public static void main(String[] args) {
