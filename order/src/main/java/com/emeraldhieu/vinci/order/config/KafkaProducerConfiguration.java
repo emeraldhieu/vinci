6f480fac6ec03c1c82720f33f47eee2d05b0c61f
Hieu <emerald.hieu@gmail.com>
2022-12-11 18:19:43 +0700

    Configure Spring Kafka in two ways; using YAML and Java

---
+++ order/src/main/java/com/emeraldhieu/vinci/order/config/KafkaProducerConfiguration.java
@@ -0,0 +1,66 @@
+package com.emeraldhieu.vinci.order.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.springframework.boot.autoconfigure.kafka.DefaultKafkaProducerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.ProducerListener;
+
+import java.util.Map;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class KafkaProducerConfiguration {
+
+    private final KafkaProperties kafkaProperties;
+
+    /**
+     * Try customizing autoconfigured {@link DefaultKafkaProducerFactory},
+     * meaning configuring producer using Java code.
+     */
+    @Bean
+    public DefaultKafkaProducerFactoryCustomizer defaultKafkaProducerFactoryCustomizer() {
+        return producerFactory -> {
+            Map<String, Object> additionalConfigs = Map.of(
+                ProducerConfig.RETRIES_CONFIG, 5
+            );
+            producerFactory.updateConfigs(additionalConfigs);
+        };
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory producerFactory) {
+        KafkaTemplate<String, String> kafkaTemplate = new KafkaTemplate<>(producerFactory);
+        kafkaTemplate.setProducerListener(new ProducerListener<>() {
+            @Override
+            public void onSuccess(ProducerRecord<String, String> producerRecord, RecordMetadata recordMetadata) {
+                log.info("Sent a record");
+            }
+
+            @Override
+            public void onError(ProducerRecord<String, String> producerRecord, RecordMetadata recordMetadata, Exception exception) {
+                log.info("Fail to send a record");
+            }
+        });
+        return kafkaTemplate;
+    }
+
+    /**
+     * Create topic "orders" if not existed.
+     */
+    @Bean
+    public NewTopic orderTopic() {
+        return new NewTopic(kafkaProperties.getTopic(),
+            kafkaProperties.getPartitions(),
+            (short) kafkaProperties.getReplicationFactor());
+    }
+}
---
+++ order/src/main/java/com/emeraldhieu/vinci/order/config/KafkaProperties.java
@@ -0,0 +1,14 @@
+package com.emeraldhieu.vinci.order.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "application.kafka")
+@Data
+public class KafkaProperties {
+    private String bootstrapAddress;
+    private String topic;
+    private int partitions;
+    private int replicationFactor;
+    private String groupId;
+}
